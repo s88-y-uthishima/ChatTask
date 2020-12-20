5a420d5cd782ed3c4ac22797a33e33d4813b5aa9
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-20 13:44:18 +0900

    接続ルームのメッセージのみ表示

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,6 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "room_channel"
+    stream_from "room_channel_#{params['room']}"
   end
 
   def unsubscribed
@@ -10,6 +10,6 @@ class RoomChannel < ApplicationCable::Channel
   def speak(data)
     # jsで実行されたspeakのmessageを受け取り、room_channelのreceivedにブロードキャストする
     # ActionCable.server.broadcast 'room_channel', message: data['message']
-    Message.create! content: data['message']
+    Message.create! content: data['message'],user_id: current_user.id, room_id: params['room']
   end
 end

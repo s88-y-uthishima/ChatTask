8e7aa0618f0d0dba5dcacd582ce99556f3458ca8
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-28 23:58:27 +0900

    roomモデルのテストコード

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
-
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '新規ルーム作成' do
+    it '店舗名があれば作成できる' do
+      room = Room.new(name: '○○店')
+      expect(room).to be_valid
+    end
+    it '店舗名が空だと作成できない' do
+      room = Room.new(name: '')
+      room.valid?
+      expect(room.errors.full_messages).to include("店舗名を入力してください")
+    end
+  end
 end
